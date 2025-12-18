7b3375f44a8b42bc89aff388950c361feef6b2da
tamilorea1 <tamiloreakinfemi@gmail.com>
2025-12-17 19:06:05 -0700

    begun my project. Already connected prisma to my database. Next is to connect next js to prisma

---
+++ mybudgetpal2025/app/page.js
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return(
+    <div>
+      Hello beginning phase, tami here
+    </div>
+  )
+}
---
+++ mybudgetpal2025/prisma/config/prisma.config.ts
@@ -0,0 +1,9 @@
+// prisma.config.ts
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  datasource: {
+    url: env("DATABASE_URL"), 
+  },
+});
